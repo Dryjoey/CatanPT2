74d68aafbeec19513d1338ad7d0f34dff9090185
MichaJLaptop <michajanssen93@gmail.com>
2019-10-21 09:48:07 +0200

    Place Method; PlaceRed                        = Finished | Needs Testing PlaceRest                       = Finished | Need Testing GetNewPositionValue  = Finished | Needs Testing RemoveAdjecent          = Finished | Needs Testing

--- Models/Tile.cs
+++ Models/Tile.cs
@@ -10,6 +10,13 @@ namespace Models
         public string tilecategorie; 
         public int chip;
         public string placement;
+        /*
+         * Subject to change towards actuall DataValues
+         * Please check and manifest working type DataValues further.
+         * DAT BETEKENT: Tile voldoet nog niet aan alle data eisen die wij hebben uitgezet. moet veranderd worden.
+        */ 
+        public string Resource { get; set; }
+        public int Position { get; set; }
 
     }
 }
