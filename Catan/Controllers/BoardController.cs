13bc2465669204f70e53f504b047ed94304f0a53
Rick Vrolings <416994@student.fontys.nl>
2019-10-22 10:43:57 +0200

    Renamed Board View to Board, added a button on index to navigate to board view, Put link in board controller to navigate to the right page

--- Catan/Controllers/BoardController.cs
+++ Catan/Controllers/BoardController.cs
@@ -10,7 +10,7 @@ namespace Catan.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            return View("Views/Board/Board.cshtml");
         }
     }
 }
