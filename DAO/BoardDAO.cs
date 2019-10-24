847da8675d19473ef368d5682b1b34d6df4f7a2e
Rick Vrolings <416994@student.fontys.nl>
2019-10-24 15:29:30 +0200

    Deleted non working DAO components

--- DAO/BoardDAO.cs
+++ DAO/BoardDAO.cs
@@ -7,67 +7,6 @@ namespace DAO
 {
     public class BoardDAO: DAO
     {
-        List<Board> board = new List<Board>();
-
-        public List<Board> GetAllBoards()
-        {
-            con.Open();
-            // where needs to be added
-            string query = "SELECT * FROM Board WHERE UserId = @userid";
-            List<Board> result = new List<Board>();
-
-            using (SqlCommand command = new SqlCommand(query, con))
-            {
-                command.ExecuteNonQuery();
-
-                SqlDataReader read = command.ExecuteReader();
-
-                while (read.Read())
-                {
-                    result.Add(new Board(read.GetInt32(0), read.GetInt32(1)));
-                }
-
-                con.Close();
-
-                return result;
-            }
-        }
-        public void AddEntity(Board board)
-        {
-            con.Open();
-
-            string query =
-
-                "INSERT INTO Board(Id,UserId)";
-
-            using (SqlCommand command = new SqlCommand(query, con))
-            {
-                command.Parameters.AddWithValue("@Id", board.Id);
-                command.Parameters.AddWithValue("@UserId", board.UserId);
-
-                command.ExecuteNonQuery();
-
-                con.Close();
-            }
-            
-        }
-
-        public void DeleteBoard(Board board)
-        {
-            con.Open();
-
-            string query = 
-                "DELETE FROM Board WHERE Id = @Id";
-            using (SqlCommand command = new SqlCommand(query, con))
-            {
-                command.Parameters.AddWithValue("@Id", board.Id);
-
-
-                command.ExecuteNonQuery();
-
-
-                con.Close();
-            }
-        }
+     
     }
 }
