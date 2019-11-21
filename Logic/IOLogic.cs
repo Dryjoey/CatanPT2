a5dbe7c6a4cb164b92147dd8b52ee5b416b33932
MichaJLaptop <michajanssen93@gmail.com>
2019-11-21 12:07:22 +0100

    Changed subtle things to IOLogic

--- Logic/IOLogic.cs
+++ Logic/IOLogic.cs
@@ -12,8 +12,8 @@ namespace Logic
         private BoardDAO boardDao = new BoardDAO();
         private UserDAO userDao = new UserDAO();
         public void SaveBoard(Board board, int UserId) => boardDao.InsertBoard(board, UserId);
-        public List<Board> GetBoardsFromUser(int UserId) => boardDao.GetAllBoardsFromUser(UserId);
-        public void DeleteBoard(Board board) => boardDao.DeleteBoard(board.BoardId);
+        public IEnumerable<Board> GetBoardsFromUser(int UserId) => boardDao.GetAllBoardsFromUser(UserId);
+        public void DeleteBoard(int BoardId) => boardDao.DeleteBoard(BoardId);
         public Board GetBoard(int BoardId) => boardDao.GetBoard(BoardId);
         public int GetLastUser() => userDao.GetLastUser();
     }
