b4a84d3273b4ffb436b0fef8193cc502eaa06258
MichaJLaptop <michajanssen93@gmail.com>
2019-11-21 10:20:19 +0100

    Refactor BoardDAO for best usage wise.

--- DAO/BoardDAO.cs
+++ DAO/BoardDAO.cs
@@ -6,33 +6,34 @@ using System.Data.SqlClient;
 
 namespace DAO
 {
-    public class BoardDAO: DAO
+    public class BoardDAO : DAO
     {
         List<Board> boards = new List<Board>();
 
         public List<Board> GetAllBoardsFromUser(int userId)
         {
-            con.Open();
-            // where needs to be added
-            string query = "SELECT * FROM Board WHERE UserId = @userid";
-            List<Board> result = new List<Board>();
-
-            using (SqlCommand command = new SqlCommand(query, con))
+            using (SqlConnection SqlCon = con)
             {
-                command.ExecuteNonQuery();
+                // where needs to be added
+                string query = "SELECT * FROM Board WHERE UserId = @userid";
+                List<Board> boards = new List<Board>();
 
-                SqlDataReader read = command.ExecuteReader();
-
-                while (read.Read())
+                using (SqlCommand command = new SqlCommand(query, SqlCon))
                 {
-                    Board board = new Board();
-                    board.BoardId = read.GetInt32(0);
-                    result.Add(board);
+                    command.Parameters.Add("@userid", SqlDbType.Int);
+                    command.Parameters["@userId"].Value = userId;
+                    using (SqlDataReader read = command.ExecuteReader())
+                    {
+                        while (read.Read())
+                        {
+                            Board board = new Board();
+                            board.BoardId = read.GetInt32(0);
+                            boards.Add(board);
+                        }
+                    }
+                    con.Close();
                 }
-
-                con.Close();
-
-                return result;
+                return boards;
             }
         }
         public Board GetBoard(int boardId)
@@ -40,66 +41,67 @@ namespace DAO
             TilesDAO til = new TilesDAO();
             PortDAO port = new PortDAO();
             Board returnBoard = new Board();
-            con.Open();
-            // where needs to be added
-            string query = "SELECT * FROM Board WHERE Id= @Id";
-            using (SqlCommand command = new SqlCommand(query, con))
+            using (SqlConnection SqlCon = con)
             {
-                command.Parameters.Add(new SqlParameter("@Id", boardId));
-                //command.Parameters["Id"].Value = boardId;
-                command.ExecuteNonQuery();
-                SqlDataReader read = command.ExecuteReader();
-               
-                while (read.Read())
+                // where needs to be added
+                string query = "SELECT * FROM Board WHERE Id= @Id";
+                using (SqlCommand command = new SqlCommand(query, con))
                 {
-                    read.GetInt32(0);
-                    read.GetInt32(1);
+                    command.Parameters.Add(new SqlParameter("@Id", boardId));
+                    command.Parameters["@Id"].Value = boardId;
+                    //command.Parameters["Id"].Value = boardId;
+                    using (SqlDataReader read = command.ExecuteReader())
+                    {
+                        while (read.Read())
+                        {
+                            returnBoard.BoardId = read.GetInt32(0);
+                            returnBoard.UserId = read.GetInt32(1);
+                        }
+                    }
+                    con.Close();
+                    returnBoard.Tiles = til.GetAllTilesFromBoard(boardId);
+                    returnBoard.Ports = port.GetAllPortsFromBoard(boardId);
                 }
-
-                con.Close();
-                returnBoard.Tiles = til.GetAllTilesFromBoard(boardId); 
-                returnBoard.Ports = port.GetAllPortsFromBoard(boardId); 
                 return returnBoard;
-               
+
                 //daarna ga naar tiles en port en haal alle gegevens op
             }
 
         }
-        public void InsertBoard(int userId)
+        public void InsertBoard(Board board,int userId)
         {
-            //inserting board
-            con.Open();
-
-            string query =
-
-                "INSERT INTO Board(UserId)";
-
-            using (SqlCommand command = new SqlCommand(query, con))
+            using (SqlConnection SqlCon = con)
             {
-                command.Parameters.Add("@UserId", SqlDbType.Int);
-                command.Parameters["board.UserId"].Value = userId;
-    
-                command.ExecuteNonQuery();
 
-                con.Close();
+                string query = "INSERT INTO Board(UserId)";
+
+                using (SqlCommand command = new SqlCommand(query, con))
+                {
+                    command.Parameters.Add("@UserId", SqlDbType.Int);
+                    command.Parameters["@UserId"].Value = userId;
+
+                    command.ExecuteNonQuery();
+
+                    con.Close();
+                }
             }
-            
+
         }
 
         public void DeleteBoard(int boardId)
         {
-            con.Open();
-
-            string query = 
-                "DELETE FROM Board WHERE Id = @Id";
-            using (SqlCommand command = new SqlCommand(query, con))
+            using (SqlConnection SqlCon = con)
             {
-                command.Parameters.Add("@Id", SqlDbType.Int);
-                command.Parameters["board.Id"].Value = boardId;
+                string query = "DELETE FROM Board WHERE Id = @Id";
+                using (SqlCommand command = new SqlCommand(query, con))
+                {
+                    command.Parameters.Add("@Id", SqlDbType.Int);
+                    command.Parameters["board.Id"].Value = boardId;
 
-                command.ExecuteNonQuery();
+                    command.ExecuteNonQuery();
 
-                con.Close();
+                    con.Close();
+                }
             }
         }
     }
