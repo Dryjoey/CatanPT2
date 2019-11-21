29b61bda9feb49c1e54aaad5723c1bc6730b52bc
MichaJLaptop <michajanssen93@gmail.com>
2019-11-21 10:55:36 +0100

    Added IOLogic for saving and loading

---
+++ Logic/IOLogic.cs
@@ -0,0 +1,20 @@
+﻿using DAO;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logic
+{
+    class IOLogic
+    {
+
+        private BoardDAO boardDao = new BoardDAO();
+        private UserDAO userDao = new UserDAO();
+        public void SaveBoard(Board board, int UserId) => boardDao.InsertBoard(board, UserId);
+        public List<Board> GetBoardsFromUser(int UserId) => boardDao.GetAllBoardsFromUser(UserId);
+        public void DeleteBoard(Board board) => boardDao.DeleteBoard(board.BoardId);
+        public Board GetBoard(int BoardId) => boardDao.GetBoard(BoardId);
+        public int GetLastUser() => userDao.GetLastUser();
+    }
+}
