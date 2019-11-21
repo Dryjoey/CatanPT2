e401e8860159e77cf9cb59f17a0f4e32874f791b
Nino Verheijen <363529@student.fontys.nl>
2019-11-21 10:35:10 +0100

    Initial implementation of the Outsourcing

--- Catan/wwwroot/js/site.js
+++ Catan/wwwroot/js/site.js
@@ -2,3 +2,30 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//For darkmode
+document.body.style.backgroundColor = sessionStorage.getItem('bg');
+document.body.style.color = sessionStorage.getItem('cc');
+function darker() {
+    if (sessionStorage.getItem('bg') === 'rgb(94, 178, 143)') {
+
+        sessionStorage.setItem('bg', 'rgb(6, 23, 37)');
+        sessionStorage.setItem('cc', '#777');
+
+
+    }
+    else if (sessionStorage.getItem('bg') == null || undefined) {
+        sessionStorage.setItem('bg', 'rgb(6, 23, 37)');
+        sessionStorage.setItem('cc', '#777');
+
+    }
+    else if (sessionStorage.getItem('bg') === 'rgb(6, 23, 37)') {
+
+        sessionStorage.setItem('bg', 'rgb(94, 178, 143)');
+        sessionStorage.setItem('cc', '#333');
+    }
+
+    document.body.style.backgroundColor = sessionStorage.getItem('bg');
+    document.body.style.color = sessionStorage.getItem('cc');
+
+}
