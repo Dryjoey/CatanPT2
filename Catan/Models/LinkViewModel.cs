dabac5b11b380af506cbc613fc8cc3dd93cc7533
Nino Verheijen <363529@student.fontys.nl>
2019-11-28 14:20:31 +0100

    Changed Some Html propertys for better styling. Added navbar to settings and home to properly place icons and images

---
+++ Catan/Models/LinkViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catan.Models
+{
+    public class LinkViewModel
+    {
+        public string PreviousURL { get; set; }
+
+        public LinkViewModel(string previousURL)
+        {
+            PreviousURL = previousURL;
+        }
+    }
+}
