ff1a7a38f938d83ee4a0d9bc1346af024bc17a41
Nino Verheijen <363529@student.fontys.nl>
2020-01-09 09:16:47 +0100

    Added translations for ports

--- Catan/wwwroot/js/MultiLanguage.js
+++ Catan/wwwroot/js/MultiLanguage.js
@@ -20,9 +20,11 @@
         'Menu': 'Menu',
         'Library': 'Library',
         'Test': 'Test',
-        'Sheep': 'sheep',
-        'Brick': 'brick',
-		'Any': 'any',
+        'two-one sheep': '2:1 sheep',
+        'two-one brick': '2:1 brick',
+        'three-one any': '3:1 any',
+        'two-one ore': '2:1 ore',
+        'two-one wood': '2:1 wood',
 		'SaveLibText': 'Board Saved to Library!',
 
 
@@ -47,9 +49,11 @@
         'Menu': 'Menu',
         'Library': 'Collectie',
         'Test': 'Test',
-        'Sheep': 'schaap',
-        'Brick': 'baksteen',
-		'Any': 'elke',
+        'two-one sheep': '2:1 schaap',
+        'two-one brick': '2:1 baksteen',
+        'three-one any': '3:1 ieder',
+        'two-one ore': '2:1 erts',
+        'two-one wood': '2:1 hout',
 		'SaveLibText': 'Board opgeslagen in collectie!',
 
     }
